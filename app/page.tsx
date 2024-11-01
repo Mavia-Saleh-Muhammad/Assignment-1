93e400c89c6f1dafb61c2d9ae97cb3444fb5ba34
Mavia-Saleh-Muhammad <bhaimavi353@gmail.com>
2024-11-01 17:19:21 +0500

    upload

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = ()=>{
+    return(
+        <div>
+            <ul className="flex justify-center bg-slate-500 p-5 font-bold text-white back">
+                <li className="mr-20 hover:bg-yellow-600 ..."><Link href="#">Home</Link></li>
+                <li className="mr-20 hover:bg-yellow-600..."><Link href="# ">Contact</Link></li>
+                <li className="mr-20 hover:bg-yellow-600..."><Link href="#">About</Link></li>
+                
+
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
